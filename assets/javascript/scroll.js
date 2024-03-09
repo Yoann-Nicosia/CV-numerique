9bdca6d31626c96bd8bf7d80e0c071d3e4f24fd0
Yoann <yoann.nicosia@gmail.com>
2024-03-09 19:43:38 +0100

    Avancement html css

--- assets/javascript/scroll.js
+++ assets/javascript/scroll.js
@@ -1,31 +1,31 @@
-$(document).ready(function() {
-    const slideCount = $('.slide').length;
-    const slideWidth = $('.slide').width();
-    let currentPosition = 0;
-    let isScrollLocked = false;
+// $(document).ready(function() {
+//     const slideCount = $('.slide').length;
+//     const slideWidth = $('.slide').width();
+//     let currentPosition = 0;
+//     let isScrollLocked = false;
   
-    $(window).on('wheel', function(e) {
-      e.preventDefault();
+//     $(window).on('wheel', function(e) {
+//       e.preventDefault();
   
-      if (isScrollLocked) return;
+//       if (isScrollLocked) return;
   
-      isScrollLocked = true;
+//       isScrollLocked = true;
   
-      const delta = e.originalEvent.deltaY;
+//       const delta = e.originalEvent.deltaY;
   
-      if (delta > 0) {
-        currentPosition += slideWidth;
-      } else {
-        currentPosition -= slideWidth;
-      }
+//       if (delta > 0) {
+//         currentPosition += slideWidth;
+//       } else {
+//         currentPosition -= slideWidth;
+//       }
   
-      currentPosition = Math.min((slideCount - 1) * slideWidth, Math.max(0, currentPosition));
+//       currentPosition = Math.min((slideCount - 1) * slideWidth, Math.max(0, currentPosition));
       
-      $('.slides').css('transform', `translateX(-${currentPosition}px)`);
+//       $('.slides').css('transform', `translateX(-${currentPosition}px)`);
   
-      setTimeout(function() {
-        isScrollLocked = false;
-      }, 0); // Délai de 800 millisecondes entre les défilements
-    });
-  });
+//       setTimeout(function() {
+//         isScrollLocked = false;
+//       }, 0); // Délai de 800 millisecondes entre les défilements
+//     });
+//   });
   
